34588ac40c26b08fb38e5d69af555ec0a4e87fab
Jibao Mansaray <jibaomansaray@gmail.com>
2021-02-20 01:25:16 +1000

    Add handling and emitting events from the frontend

--- frontend/src/src/pages/Index.vue
+++ frontend/src/src/pages/Index.vue
@@ -1,49 +1,45 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <div>
+       {{ messages }}
+    </div>
+    <hr />
+    <q-input v-model="newMessage" @keyup="doSend">
+    </q-input>
   </q-page>
 </template>
 
 <script lang="ts">
 import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/CompositionComponent.vue';
 import { defineComponent, ref } from '@vue/composition-api';
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { ExampleComponent },
-  setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
+  setup(props, ctx) {
+
+    let newMessage = ref<String>('')
+    let messages = ref<String[]>([])
+
+    // test sending events to server
+    let doSend = (event)  => {
+      if (event.key.toString().toLowerCase() === 'enter') {
+        ctx.root.cpSocket.emit('message',  newMessage.value)
+        messages.value.push(newMessage.value)
+        newMessage.value = ''
       }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
+    }
+
+    // test handling events from server
+    ctx.root.cpSocket.on('message', (msg) => {
+      messages.value.push(msg)
+    })
+
+
+    return {
+      doSend,
+      messages,
+      newMessage
+    };
   }
 });
 </script>
