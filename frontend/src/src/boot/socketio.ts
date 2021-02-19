9fa3b7e38e0267086d74555a51bd711434c944ec
Jibao Mansaray <jibaomansaray@gmail.com>
2021-02-20 00:11:41 +1000

    Pluck the chat domain from the meta tag
    The chat domain is stored in a meta tag called
    **cp_chat_server**

--- frontend/src/src/boot/socketio.ts
+++ frontend/src/src/boot/socketio.ts
@@ -1,9 +1,12 @@
 // import something here
 import { io } from 'socket.io-client'
 
+console.log('env con', JSON.stringify(process.env), __filename)
+
 // "async" is optional;
 // more info on params: https://quasar.dev/quasar-cli/boot-files
-export default async (/* { app, router, Vue ... } */) => {
+export default async ({ Vue }/* { app, router, Vue ... } */) => {
   // something to do
-  const client = io('http://localhost:3000')
+  const chatDomain = document.querySelector("meta[name='cp_chat_server']")?.getAttribute("content") || 'http://localhost:3000'
+  const client = io(chatDomain)
 }
