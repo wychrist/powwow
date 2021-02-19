aa3b90baf1142af1d145a1391fd79d1120d77083
Jibao Mansaray <jibaomansaray@gmail.com>
2021-02-19 23:07:22 +1000

    Load dynamic configuration from .env file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,40 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import dotenv from "dotenv";
+import fs from "fs";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const envFilePath = `${__dirname}/../../.env`;
+const envFilePathFallback = `${__dirname}/../../.env.example`;
+
+if (fs.existsSync(envFilePath)) {
+  dotenv.config({
+    path: envFilePath,
+  });
+} else {
+  dotenv.config({
+    path: envFilePathFallback,
+  });
+}
+
+const PORT = process.env.CP_SERVER_PORT;
 const httpServer = createServer();
 const io = new Server(httpServer, {
-    cors: {
-        origin: '*'
-    }
-    // we are not using another websocket
-    // ws is awesome and we are sticking with it!!! :)
+  cors: {
+    origin: "*",
+  },
+  // we are not using another websocket
+  // ws is awesome and we are sticking with it!!! :)
 });
 
 io.on("connection", (socket) => {
-    console.log('we got a new connection!!!!!!!!');
+  console.log("we got a new connection!!!!!!!!");
 });
 
-httpServer.listen(3000, () => {
-    console.log('We are waiting for connection on port: ' + 3000)
+httpServer.listen(PORT, () => {
+  console.log("We are waiting for connection on port: " + PORT);
 });
