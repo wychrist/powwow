ecf1493389fa4dcdc3a35080f06b84541059ce87
Jibao Mansaray <jibaomansaray@gmail.com>
2021-02-20 11:58:05 +1000

    Unregister the socketio boot file
    This feature has been moved to an independent file at:
    lib/socketio.ts

--- frontend/chat_client/quasar.conf.js
+++ frontend/chat_client/quasar.conf.js
@@ -42,8 +42,7 @@ module.exports = configure(function (ctx) {
     boot: [
       'composition-api',
       'i18n',
-      'axios',
-      'socketio'
+      'axios'
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
