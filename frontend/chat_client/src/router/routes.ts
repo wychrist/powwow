0a113e2bf4db386e06f97de8160404a16b6bc5a6
Jibao Mansaray <jibaomansaray@gmail.com>
2021-02-20 12:01:12 +1000

    Add a desktop layout of the interface
    This is just a start. We need some awesomeness here

--- frontend/chat_client/src/router/routes.ts
+++ frontend/chat_client/src/router/routes.ts
@@ -8,6 +8,13 @@ const routes: RouteConfig[] = [
       { path: '', component: () => import('pages/Index.vue') }
     ]
   },
+  {
+    path: '/chat-desktop',
+    component: () => import('layouts/DesktopLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/chat/Index.vue') }
+    ]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
