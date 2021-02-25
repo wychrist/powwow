4651e9c4ae6bc0f9e1b4fd4d2319a066010e47f6
Jibao Mansaray <jibaomansaray@gmail.com>
2021-02-26 01:21:25 +1000

    Save the generated api token

--- backend/api/app/Models/User.php
+++ backend/api/app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected static function booted()
+    {
+        static::created(function ($user) {
+            $user->api_token = $user->createToken('CP')->plainTextToken;
+            $user->save();
+        });
+    }
 }
