c8cda2f3069dd81c51dd89244f65b596571fb742
Jibao Mansaray <jibaomansaray@gmail.com>
2021-02-26 01:18:44 +1000

    Add base module

---
+++ backend/api/Modules/CPBase/Database/Seeders/CPBaseDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\CPBase\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class CPBaseDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
