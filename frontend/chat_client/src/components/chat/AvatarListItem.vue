7f16219377a456c133ba0f0b2a2325108440a214
Daniel Newman <contact@lifeofdan.me>
2021-03-04 14:02:16 +0000

    Added an avatar list item component to show the avatar and title and subtitle.

---
+++ frontend/chat_client/src/components/chat/AvatarListItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <q-list>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar
+            color="accent" text-color="white"
+          >
+          </q-avatar>
+        </q-item-section>
+        <q-item-section
+        >
+          <q-item-label class="text-white">{{title}}</q-item-label>
+          <q-item-label caption lines="1" class="text-white">{{subTitle}}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AvatarListItem',
+  props: {
+    title: String,
+    subTitle: String
+  }
+})
+
+</script>
+
+<style>
+
+</style>
