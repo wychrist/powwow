c41f66aa10c62fbb6ca9201167f9ba7b9df4519c
Jibao Mansaray <jibaomansaray@gmail.com>
2021-03-26 16:15:54 +0000

    Fix application columns types

--- backend/realtime_server/src/migration/1616158722399-Application.ts
+++ backend/realtime_server/src/migration/1616158722399-Application.ts
@@ -1,16 +1,19 @@
 import { env } from "process";
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
+const tableName = `${env.DB_TABLE_PREFIX}application`;
+
 export class Application1616158722399 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         queryRunner.createTable(new Table({
-            name: `${env.DB_TABLE_PREFIX}application`,
+            name: tableName,
             columns: [
                 {
                     name: 'id',
-                    type: 'int',
-                    isPrimary: true
+                    type: 'varchar',
+                    isPrimary: true,
+                    generationStrategy: 'uuid'
                 },
                 {
                     name: 'name',
@@ -23,16 +26,26 @@ export class Application1616158722399 implements MigrationInterface {
                     length: '255'
                 },
                 {
-                    name: 'server',
-                    type: 'varchar',
-                    length: '255'
+                    name: "domain",
+                    type: "varchar",
+                    length: "512"
+                },
+                {
+                    name: "key",
+                    type: "varchar",
+                    length: "256"
+                },
+                {
+                    name: "secret",
+                    type: "varchar",
+                    length: "256"
                 }
             ]
         }), true)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.dropTable('application', true);
+        queryRunner.dropTable(tableName, true);
     }
 
 }
