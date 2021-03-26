6cfed1b8725d8878baae2ca76dddd9e76d7a5359
Jibao Mansaray <jibaomansaray@gmail.com>
2021-03-26 16:18:33 +0000

    Add more interfaces

--- backend/realtime_server/src/type/Interfaces.ts
+++ backend/realtime_server/src/type/Interfaces.ts
@@ -1,11 +1,24 @@
-interface IApplication {
+import { Controler } from "../controller/Controller";
+
+export interface IApplication {
     id?: number,
     name: string,
     server: string,
     status: string
 }
 
-interface ValidationResult {
+export interface ValidationResult {
     success: boolean,
     message?: string
+}
+
+export interface IRoute {
+        method: string,
+        route: string,
+        controller: Function, 
+        action: string
+}
+
+export interface ISettingEntry {
+    [name: string]: string
 }
