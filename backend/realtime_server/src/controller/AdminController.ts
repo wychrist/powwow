dc269f0014281091d3a3295e086090e5bde8dfc8
Jibao Mansaray <jibaomansaray@gmail.com>
2021-03-29 10:43:41 +0000

    Add application delete and restore methods
    Did some general clean up

--- backend/realtime_server/src/controller/AdminController.ts
+++ backend/realtime_server/src/controller/AdminController.ts
@@ -24,6 +24,25 @@ export class AdminController extends Controler {
         })
     }
 
+    async getAnApplication(request: Request, response: Response): Promise<Application> {
+        return new Promise((resolve, reject) => {
+            this.isBearerEqual(request, this.adminToken, async (isValid: boolean) => {
+                if (isValid) {
+                    try {
+                        const app = await this.appRepository.findOneOrFail(request.params.id);
+                        resolve(app)
+                    } catch (error) {
+                        this.return404(response, error.message)
+                        reject(error)
+                    }
+                } else {
+                    this.returnInvalidToken(response)
+                    reject()
+                }
+            })
+        });
+    }
+
     async createApplication(request: Request, response: Response): Promise<Application> {
         return new Promise(async (resolve, _) => {
             this.isBearerEqual(request, this.adminToken, async (isValid: boolean) => {
@@ -40,7 +59,7 @@ export class AdminController extends Controler {
                         resolve(this.appRepository.save(app));
 
                     } catch (error) {
-                        response.status(400).end(error.message)
+                        response.status(400).json(this.buildResponseMessage(error.message))
                     }
 
                 } else {
@@ -52,7 +71,6 @@ export class AdminController extends Controler {
 
     async updateApplication(request: Request, response: Response): Promise<Application> {
         return new Promise(async (resolve, _) => {
-            console.log('in....')
             this.isBearerEqual(request, this.adminToken, async (isValid: boolean) => {
                 if (isValid) {
                     try {
@@ -64,13 +82,50 @@ export class AdminController extends Controler {
                         this.appRepository.save(app)
                         resolve(app)
                     } catch (error) {
-                        response.status(400).end(error.message)
+                        response.status(400).json(this.buildResponseMessage(error.message));
                     }
                 } else {
                     this.returnInvalidToken(response)
                 }
             })
         })
+    }
+
+    async deleteApplication(request: Request, response: Response): Promise<Application> {
+        return new Promise(async (resolve, _) => {
+            this.isBearerEqual(request, this.adminToken, async (isValid: boolean) => {
+                if (isValid) {
+                    try {
+                        let app = await this.appRepository.findOneOrFail(request.params.id)
+                        this.appRepository.softDelete(app.id);
+                        resolve(app);
+                    } catch (error) {
+                        this.return404(response, error.message);
+                    }
+                } else {
+                    this.returnInvalidToken(response)
+                }
+            });
+        });
+    }
+
+    async restoreApplication(request: Request, response: Response): Promise<Application> {
+        return new Promise(async (resolve, _) => {
+            this.isBearerEqual(request, this.adminToken, async (isValid: boolean) => {
+                if (isValid) {
+                    try {
+                        const result = await this.appRepository.restore(request.params.id);
+                        const app = await this.appRepository.findOneOrFail(result.affected);
+                        resolve(app);
+                    } catch (error) {
+                        response.status(404).end({ message: error.message })
+                    }
+
+                } else {
+                    this.returnInvalidToken(response);
+                }
+            });
+        });
     }
 
     private validate(data: object, app: Application = null): object {
@@ -84,14 +139,14 @@ export class AdminController extends Controler {
                 }
             },
             domain: (val: string) => {
-                if (val && val.indexOf('http')) {
+                if (val && val.indexOf('http') !== -1) {
                     clean['domain'] = val
                 } else if (!app || !app.id) {
                     throw Error('valid domain is required');
                 }
             },
             status: (val: string = null) => {
-                if (val && val.trim())  {
+                if (val && val.trim()) {
                     clean['status'] = val
                 } else if (!app || !app.id) {
                     throw Error('status is required');
