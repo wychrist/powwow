5edea3932c3048bf072707a646b79806bd00e482
Jibao Mansaray <jibaomansaray@gmail.com>
2021-03-30 14:25:08 +0000

    Start adding event dispatcher methods

--- backend/realtime_server/src/pusher/PusherApplication.ts
+++ backend/realtime_server/src/pusher/PusherApplication.ts
@@ -15,6 +15,8 @@ export class PusherApplication {
   authenticate(socket: Socket) {
 
     console.log(`socket connected on ${this.app.name}`, socket.id)
+    this.acceptConnection(socket)
+
     // wait for a specified timeout and see if the client has joined a private room
 
     socket.on('disconnect', (reason) => {
@@ -24,5 +26,27 @@ export class PusherApplication {
     socket.on('disconnecting', (reason) => {
       console.log('client disconnecting', reason);
     });
+
+    socket.on('pusher:ping', () => {
+      socket.emit('pusher:pong')
+      console.log('we got a ping from the client', Date.now())
+    })
   }
+
+  acceptConnection(socket: Socket) {
+    socket.emit('pusher:connection_established', JSON.stringify({
+      socket_id: socket.id,
+      activitiy_timeout: 120
+    }))
+    console.log("we got a new connection: " + socket.id);
+  }
+
+  sendError(socket: Socket, code: number, message: string) {
+    socket.emit('pusher:error', JSON.stringify({
+      code,
+      message
+    }))
+  }
+
+
 }
