0ffa24690f0b2b54cfe625388a5a9e779a25a19b
Jibao Mansaray <jibaomansaray@gmail.com>
2021-03-30 14:22:25 +0000

    Remove unused code

--- frontend/chat_client/src/lib/socketio.ts
+++ frontend/chat_client/src/lib/socketio.ts
@@ -1,7 +1,1 @@
-import { io } from 'socket.io-client'
-
-const chatDomain = document.querySelector('meta[name=\'cp_chat_server\']')?.getAttribute('content') || 'http://realtime.test:3000'
-// const ioClient =  io(chatDomain)
-
-
-export const client = io(chatDomain)
+export const chatDomain = document.querySelector('meta[name=\'cp_chat_server\']')?.getAttribute('content') || 'http://realtime.test:3000'
