e4e7d74f35f5ca0de5605cb22f48594c48a64d0d
Jibao Mansaray <jibaomansaray@gmail.com>
2021-03-31 14:53:15 +0000

    Add a test route for testing auth hash generation

--- backend/api/routes/api.php
+++ backend/api/routes/api.php
@@ -17,3 +17,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('pusher/auth', function () {
+    $appKey = 'f97b3be0c78ac08b953aa486565786c120e7de03914df750612b9c52afce7528';
+    $appSecret = '91a6eda65fd1d51c68f0b63f8127652ad376355a40e06c6ee7860bd9470bb695';
+    $socketId = request()->input('socket_id', '');
+    $channelName = request()->input('channel_name', '');
+
+    if (strstr($channelName, 'presence-') !== false) {
+        $channelData = json_encode(['user_id' => 1]);
+
+        $signature = hash_hmac('sha256', $socketId . ':' . $channelName . ':' . $channelData, $appSecret);
+        return [
+            'auth' => $appKey . ':' . $signature,
+            'channel_data' => $channelData
+        ];
+    } else {
+
+        $signature = hash_hmac('sha256', $socketId . ':' . $channelName, $appSecret);
+        return [
+            'auth' => $appKey . ':' . $signature
+        ];
+    }
+});
