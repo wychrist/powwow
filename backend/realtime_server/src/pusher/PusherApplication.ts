18d80b11d51e645dfdd209586ef1f10bd62cfc76
Jibao Mansaray <jibaomansaray@gmail.com>
2021-03-31 13:45:32 +0000

    Disconnect the client if channel auth fails
    - We are also disconnecting the client/socket base
      on the current error code

--- backend/realtime_server/src/pusher/PusherApplication.ts
+++ backend/realtime_server/src/pusher/PusherApplication.ts
@@ -16,25 +16,16 @@ export class PusherApplication {
     this.app = app;
   }
 
-  authenticate(socket: Socket) {
+  authenticate(socket: Socket & PusherSocket) {
+
+    socket._pusherChannels = new Set<string>()
 
     console.log(`socket connected on ${this.app.name}`, socket.id)
-    this.acceptConnection(socket)
 
     // wait for a specified timeout and see if the client has joined a private room
-
-    socket.on('disconnect', (reason) => {
-      console.log('client disconnected', reason);
-    });
-
-    socket.on('disconnecting', (reason) => {
-      console.log('client disconnecting', reason);
-    });
-
-    socket.on('pusher:ping', () => {
-      socket.emit('pusher:pong')
-      console.log('we got a ping from the client', Date.now())
-    })
+    this.doConnectionAuth(socket)
+    this.acceptConnection(socket)
+    this.registerEventHandlers(socket)
   }
 
   acceptConnection(socket: Socket) {
@@ -45,12 +36,48 @@ export class PusherApplication {
     console.log("we got a new connection: " + socket.id);
   }
 
-  sendError(socket: Socket, code: number, message: string) {
-    socket.emit('pusher:error', JSON.stringify({
-      code,
-      message
-    }))
+  private registerEventHandlers(socket: Socket) {
+    // handle client disconnet event
+    socket.on('disconnect', (reason) => {
+      console.log('client disconnected', reason);
+    });
+
+    // Handle client disconnecting event
+    socket.on('disconnecting', (reason) => {
+      console.log('client disconnecting', reason);
+    });
+
+    // handle pusher ping event
+    socket.on('pusher:ping', () => {
+      socket.emit('pusher:pong')
+      console.log('we got a ping from the client', Date.now())
+    })
   }
 
+  private doConnectionAuth(socket: Socket & PusherSocket) {
+    setTimeout(() => {
+      if (socket._pusherChannels.size === 0) {
+        Pusher.sendError(socket, Pusher.errorCode[4009])
+      } else {
+        let done = true;
+        let valid = false;
+        const interator = socket._pusherChannels.values()
+        do {
+          const result = interator.next()
+          done = result.done
+          console.log('validating using the current channel', result.value)
+          if (result.value && (result.value.indexOf('private-') === 0 || result.value.indexOf('presence-') === 0)) {
+            valid = true
+            break;
+          }
+        } while (!done)
+
+        if (!valid) {
+          Pusher.sendError(socket, Pusher.errorCode[4009])
+        }
+      }
+    }, 30000);
+
+  }
 
 }
