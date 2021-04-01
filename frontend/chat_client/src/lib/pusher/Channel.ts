de87a8ecfdc39198b674ca23ff9bf8b9f64f93b5
Jibao Mansaray <jibaomansaray@gmail.com>
2021-04-01 15:48:23 +0000

    Fix event handling and dispatching

--- frontend/chat_client/src/lib/pusher/Channel.ts
+++ frontend/chat_client/src/lib/pusher/Channel.ts
@@ -1,26 +1,48 @@
 import { Socket } from 'socket.io-client'
-import { IChannel, EventHandler } from './model'
+import { IChannel, ClientEventHandler } from './model'
 
 export class Channel implements IChannel {
 
   private name: string;
   private client;
+  private eventMap: Map<string, ClientEventHandler[]>;
 
   constructor(name: string, client: Socket | null) {
     this.name = name
     this.client = client;
+    this.eventMap = new Map<string, ClientEventHandler[]>()
+
+    this.client?.on('pusher_internal:subscription_succeeded', (data: string) => {
+      const payload = JSON.parse(data) as { channel: string }
+      if (payload.channel === this.name) {
+        this.dispatch('succeeded', payload)
+        console.log('subscribing was successful!!!!!')
+      }
+    })
+
   }
 
   trigger(event: string, data: string): IChannel {
-    this.client?.emit(event, data);
+    this.client?.emit(`client-${event}`, data);
     return this;
   }
 
-  bind(event: string, callback: EventHandler): IChannel {
-    this.client?.on(`${this.name}:${event}`, callback);
+  bind(event: string, callback: ClientEventHandler): IChannel {
+    this.eventMap.set(event, [...this.eventMap.get(event) || [], callback])
+
+    switch (event) {
+      case 'succeeded':
+        // do nothing
+        break;
+      default:
+        this.client?.on(`${this.name}:${event}`, (data) => {
+          this.trigger(event, data)
+        });
+    }
+
     return this;
   }
-  unbind(event?: string, handler?: EventHandler): IChannel {
+  unbind(event?: string, handler?: ClientEventHandler): IChannel {
 
     if (event && handler) {
 
@@ -35,4 +57,9 @@ export class Channel implements IChannel {
     return this;
   }
 
+  private dispatch(event: string, data: unknown) {
+    this.eventMap.get(event)?.forEach((callback) => {
+      callback(data)
+    })
+  }
 }
