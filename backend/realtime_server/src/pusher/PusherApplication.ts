7ce0151722c44ba3885675a9f95a02a76cbea58b
Jibao Mansaray <jibaomansaray@gmail.com>
2021-04-01 15:50:45 +0000

    Reflect pusher class name change
    Move most of the code to the puser server class

--- backend/realtime_server/src/pusher/PusherApplication.ts
+++ backend/realtime_server/src/pusher/PusherApplication.ts
@@ -1,7 +1,7 @@
 
 import { Namespace, Socket } from "socket.io";
 import { Application } from "../entity/Application";
-import { Pusher } from "./Pusher";
+import { PusherServer } from "./PusherServer";
 import { PusherSocket } from '../type/Interfaces'
 
 export class PusherApplication {
@@ -15,8 +15,6 @@ export class PusherApplication {
   }
 
   authenticate(socket: PusherSocket) {
-    socket._pusherChannels = new Set<string>();
-
     console.log(`socket connected on ${this.app.name}`, socket.id)
 
     this.doConnectionAuth(socket); // wait for a specified timeout and see if the client has joined a private room
@@ -33,7 +31,7 @@ export class PusherApplication {
     console.log("we got a new connection: " + socket.id);
   }
 
-  private registerEventHandlers(socket: Socket) {
+  private registerEventHandlers(socket: PusherSocket) {
     // handle client disconnet event
     socket.on('disconnect', (reason) => {
       console.log('client disconnected', reason);
@@ -47,31 +45,49 @@ export class PusherApplication {
     // handle pusher ping event
     socket.on('pusher:ping', () => {
       socket.emit('pusher:pong')
-      console.log('we got a ping from the client', Date.now())
     })
+
+
+    socket.on('pusher:unsubscribe', (data: string) => {
+      PusherServer.removeClientFromChannel(socket, data)
+    })
+
+    socket.on('pusher:subscribe', (data: string) => {
+      if (PusherServer.addClientToChannel(socket, this.app, data)) {
+        // @todo log the user as connected
+      } else {
+        // @todo ...
+      }
+    })
+  }
+
+  private hasJoinAuthChannel(socket: PusherSocket): boolean {
+    let valid = false;
+    if (socket._pusherChannels.size === 0) {
+      PusherServer.sendError(socket, PusherServer.errorCode[4009])
+    } else {
+      let done = true;
+      const interator = socket._pusherChannels.values()
+      do {
+        const result = interator.next()
+        done = result.done
+        if (result.value && (result.value.indexOf('private-') === 0 || result.value.indexOf('presence-') === 0)) {
+          valid = true
+          break;
+        }
+      } while (!done)
+    }
+
+
+    console.log(`socker ${socket.id} has right to ?`, valid)
+
+    return valid
   }
 
   private doConnectionAuth(socket: PusherSocket) {
     setTimeout(() => {
-      if (socket._pusherChannels.size === 0) {
-        Pusher.sendError(socket, Pusher.errorCode[4009])
-      } else {
-        let done = true;
-        let valid = false;
-        const interator = socket._pusherChannels.values()
-        do {
-          const result = interator.next()
-          done = result.done
-          console.log('validating using the current channel', result.value)
-          if (result.value && (result.value.indexOf('private-') === 0 || result.value.indexOf('presence-') === 0)) {
-            valid = true
-            break;
-          }
-        } while (!done)
-
-        if (!valid) {
-          Pusher.sendError(socket, Pusher.errorCode[4009])
-        }
+      if (!this.hasJoinAuthChannel(socket)) {
+        PusherServer.sendError(socket, PusherServer.errorCode[4009])
       }
     }, 30000);
 
