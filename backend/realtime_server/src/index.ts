08fb418fdc970db9be1258bcd1c6f2d189b73a87
Jibao Mansaray <jibaomansaray@gmail.com>
2021-04-03 13:57:06 +0000

    Pass socket io instance and active apps collection to controllers

--- backend/realtime_server/src/index.ts
+++ backend/realtime_server/src/index.ts
@@ -36,7 +36,7 @@ createConnection().then(async connection => {
     // register express routes from defined application routes
     Routes.forEach(route => {
         (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next);
+            const result = (new (route.controller as any)(io, activeApplications))[route.action](req, res, next);
             if (result instanceof Promise) {
                 result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
                     .catch((error) => {
