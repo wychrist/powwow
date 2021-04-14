aff2cdbc8dace22794d6bf86e18a96b9a9c5767e
Jibao Mansaray <jibaomansaray@gmail.com>
2021-04-14 14:39:40 +0000

    Formatting

--- backend/realtime_server/src/routes.ts
+++ backend/realtime_server/src/routes.ts
@@ -5,60 +5,67 @@ import { IRoute } from './type/Interfaces'
 
 
 const appRoutes: IRoute[] = [
-    {
-        method: 'get',
-        route: '/api/app/:key',
-        controller: ApplicationController,
-        action: 'get'
-    },
-    {
-        method: 'post',
-        route: '/api/app/:key/trigger',
-        controller: ApplicationController,
-        action: 'trigger'
-    }
+  {
+    method: 'get',
+    route: '/api/app/:key',
+    controller: ApplicationController,
+    action: 'get'
+  },
+  {
+    method: 'post',
+    route: '/api/app/:key/trigger',
+    controller: ApplicationController,
+    action: 'trigger'
+  },
+  {
+
+    method: 'post',
+    route: '/apps/:id/events',
+    controller: ApplicationController,
+    action: 'triggerEvent'
+  }
 ];
 
 const adminRoutes: IRoute[] = [
-    {
-        method: 'get',
-        route: '/api/admin/applications',
-        controller: AdminController,
-        action: 'getApplications'
-    },
-    {
-        method: 'get',
-        route: '/api/admin/applications/:id',
-        controller: AdminController,
-        action: 'getAnApplication'
-    },
-    {
-        method: 'post',
-        route: '/api/admin/applications',
-        controller: AdminController,
-        action: 'createApplication'
-    },
-    {
-        method: 'put',
-        route: '/api/admin/applications/:id',
-        controller: AdminController,
-        action: 'updateApplication'
-    },
-    {
-        method: 'delete',
-        route: '/api/admin/applications/:id',
-        controller: AdminController,
-        action: 'deleteApplication'
-    },
-    {
-        method: 'put',
-        route: '/api/admin/applications/restore/:id',
-        controller: AdminController,
-        action: 'restoreApplication'
-    }
+  {
+    method: 'get',
+    route: '/api/admin/applications',
+    controller: AdminController,
+    action: 'getApplications'
+  },
+  {
+    method: 'get',
+    route: '/api/admin/applications/:id',
+    controller: AdminController,
+    action: 'getAnApplication'
+  },
+  {
+    method: 'post',
+    route: '/api/admin/applications',
+    controller: AdminController,
+    action: 'createApplication'
+  },
+  {
+    method: 'put',
+    route: '/api/admin/applications/:id',
+    controller: AdminController,
+    action: 'updateApplication'
+  },
+  {
+    method: 'delete',
+    route: '/api/admin/applications/:id',
+    controller: AdminController,
+    action: 'deleteApplication'
+  },
+  {
+    method: 'put',
+    route: '/api/admin/applications/restore/:id',
+    controller: AdminController,
+    action: 'restoreApplication'
+  }
 ];
 
 export const Routes = [
-    ...adminRoutes,
-    ...appRoutes,
+  ...adminRoutes,
+  ...appRoutes,
 ];
