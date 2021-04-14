44acb033134fc51bff43d1039518acdf5a981dfc
Jibao Mansaray <jibaomansaray@gmail.com>
2021-04-14 14:37:08 +0000

    wrote down a rough implementation how to has the data

--- backend/api/routes/api.php
+++ backend/api/routes/api.php
@@ -19,8 +19,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('pusher/auth', function () {
-    $appKey = 'f3c81d3408ab29d549f6ceba187b96eb862814041773574ee0884a4e8c09cb8e';
-    $appSecret = 'e98d39aa17768e9c50efaabe787449b38cc0a6721e64d5db57837fab7562a56b';
+    $appKey = '6b48260d49de1e597d2dc78fe92b970b94ea1aca47007b8c098bb0282005dc71';
+    $appSecret = '2aa203c260cb6e5050e2f2c127991692ed18b40da5cc7e7cb87b8a2f20463058';
     $socketId = request()->input('socket_id', '');
     $channelName = request()->input('channel_name', '');
 
@@ -45,4 +45,29 @@ Route::post('pusher/auth', function () {
             'auth' => $appKey . ':' . $signature
         ];
     }
+});
+
+
+Route::get('send-message', function(){
+    $data = json_encode([
+        'foo' => 'bar'
+    ]);
+
+    $appKey = '6b48260d49de1e597d2dc78fe92b970b94ea1aca47007b8c098bb0282005dc71';
+    $appSecret = '2aa203c260cb6e5050e2f2c127991692ed18b40da5cc7e7cb87b8a2f20463058';
+    $appId = 'b30001ae-63a2-4b14-9be7-ee6ce60551a8';
+    $authVersion = '1.0';
+
+    $payload = [
+        'name' => 'event_name',
+        'channel' => 'channel_name',
+        'data' => $data
+    ];
+    $ts = time();
+    $bodyMd5 = md5($data);
+
+    $str = "POST\n/apps/{$appId}/events\nauth_key={$appKey}&auth_timestamp={$ts}&auth_version={$authVersion}&body_md5={$bodyMd5}";
+    $str .= '&auth_signature='. hash_hmac('sha256', $str, $appSecret);
+
+    //POST\n/apps/3/events\nauth_key=278d425bdf160c739803&auth_timestamp=1353088179&auth_version=1.0&body_md5=ec365a775a4cd0599faeb73354201b6f
 });
