914ffb13502e1a7bbf21bc29fccecca5b66663f1
Jibao Mansaray <jibaomansaray@gmail.com>
2021-11-16 13:42:09 +0000

    Add a header image to the about page

--- projects/placeholder/app/content/pages/about-us.php
+++ projects/placeholder/app/content/pages/about-us.php
@@ -5,6 +5,8 @@ if(!defined('WYCHRIST_INIT')){
 
 
 $content = [
-  'title' => 'About us'
+  'title' => 'About us',
+  'aboutpage_title' => 'This is us',
+  'bg_image' => '/assets/base_theme/img/bg8.jpg'
 ];
 serve_template('templates/about_us_template', $content);
