1936f8201bb8e45d024a1a7d4fa26e0ec8abd1b1
Jibao Mansaray <jibaomansaray@gmail.com>
2021-11-29 09:18:17 +0000

    Remove the blank target

--- projects/placeholder/app/theme/paper_v2/sections/header.php
+++ projects/placeholder/app/theme/paper_v2/sections/header.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg fixed-top navbar-transparent bg-info" color-on-scroll="200">
   <div class="container">
     <div class="navbar-translate">
-      <a class="navbar-brand" href="./" rel="tooltip" title=<?= $page->title ?> data-placement="bottom" target="_blank">
+      <a class="navbar-brand" href="/" rel="tooltip" title=<?= $page->title ?> data-placement="bottom">
         <?= $page->title ?>
       </a>
       <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
