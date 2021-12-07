0ab93607624a00fb0ae0c2fb79ad1c2f25ec51a8
Christopher Kelly <w0072196@umail.uqs.edu.au>
2021-12-08 00:41:12 +1000

    Added styling section to paper_V2 theme to show alternating image text.

--- projects/placeholder/app/content/data/paper_2/about_us_template.php
+++ projects/placeholder/app/content/data/paper_2/about_us_template.php
@@ -7,7 +7,7 @@ $content = [
   'subtitle' => 'Who we are, What we do',
   'intro' => 'intro string',
   'content' => 'content for body of page',
-  'image' => './assets/paper_theme_v2/img/emily-morter-8xAA0f9yQnE-unsplash.jpg',
+  'image' => './assets/img/emily-morter-8xAA0f9yQnE-unsplash-cropped.webp',
   'images' => [
     'first' => '',
     'second' => ''
@@ -16,17 +16,37 @@ $content = [
 
 $page = new Page($content);
 
-$section1 = new Page(['title' => 'Who we are']);
-$section1Page1Content = ['content' => 'Who we are section, page 1', 'image' => './assets/img/worship_hall_1.webp'];
-$section1Page2Content = ['content' => 'Who we are section, page 2', 'image' => '/assets/img/worship_hall_2.webp'];
+$whoWeAre = new Page(['title' => 'Who we are']);
+$whoWeArePage1Content = ['content' => 'Who we are section, page 1', 'image' => './assets/img/worship_hall_1.webp'];
+$whoWeArePage2Content = ['content' => 'Who we are section, page 2', 'image' => './assets/img/worship_hall_2.webp'];
 
-$section1->children = [
-  new Page($section1Page1Content),
-  new Page($section1Page2Content),
+$whoWeAre->children = [
+  new Page($whoWeArePage1Content),
+  new Page($whoWeArePage2Content),
+];
+
+$communityInvolvement = new Page(['title' => 'Community Involvement']);
+$communityInvolvementPage1Content = ['title' => 'Tony\'s Kitchen', 'content' => 'Blankets and jumpers for the homeless', 'image' => './assets/img/placeholder.jpg'];
+$communityInvolvementPage2Content = ['title' => 'Operation Christmas Child', 'content' => 'Shoe boxes for impoverished children full of essential items and items to love. Packed and ready to ship for Christmas.', 'image' => './assets/img/Operation_chrismas_Child.jpg'];
+
+$communityInvolvement->children = [
+  new Page($communityInvolvementPage1Content),
+  new Page($communityInvolvementPage2Content),
+];
+
+$history = new Page(['title' => 'History']);
+$historyPage1Content = ['title' => 'Worshiping in the Homes', 'content' => 'Worshiping in homes.', 'image' => './assets/img/worship_home_1.jpg'];
+$historyPage2Content = ['title' => 'Worshiping in the Hall', 'content' => 'Worshiping in the Wyreema Community Hall.', 'image' => './assets/img/worship_hall_3.jpg'];
+
+$history->children = [
+  new Page($historyPage1Content),
+  new Page($historyPage2Content),
 ];
 
 
 return [
   'page' => $page,
-  'section1' => $section1
+  'whoWeAre' => $whoWeAre,
+  'communityInvolvement' => $communityInvolvement,
+  'history' => $history,
 ];
--- projects/placeholder/app/theme/paper_v2/templates/about_us_template.php
+++ projects/placeholder/app/theme/paper_v2/templates/about_us_template.php
@@ -5,7 +5,6 @@ $page->google = $this->settings('socials.google');
 $page->twitter = $this->settings('socials.twitter');
 $page->github = $this->settings('socials.github');
 $page->email = $this->settings('socials.email');
-$community_involvement =  include_once content_dir('data/community_involvement.php');
 
 // Layout
 $this->layout('layouts/landing', compact('page'))
@@ -14,56 +13,9 @@ $this->layout('layouts/landing', compact('page'))
 
 
 <?php $this->start('section1'); ?>
-<?php if (isset($section1)) : ?>
-  <div class="container">
-    <h1><?= $section1->title ?></h1>
-    <?php if ($section1->children) : ?>
-      <?php foreach ($section1->children as $index => $child) :  ?>
-        <?php if ($index && $index % 2 != 0) :  ?>
-          <div class="row">
-            <img class="col-md-6" style="padding: 15 px;" src="<?= $child->image ?>" alt="Worship at Wyreema Hall">
-            <div class="col-md-6">
-              <p><?= $child->content ?></p>
-            </div>
-          </div>
-        <?php else : ?>
-          <div class="row">
-            <div class="col-md-6">
-              <p><?= $child->content ?></p>
-            </div>
-            <img class="col-md-6" style="padding: 15 px;" src="<?= $child->image ?>" alt="Worship at Wyreema Hall">
-          </div>
-
-        <?php endif; ?>
-
-      <?php endforeach  ?>
-  </div>
-<?php endif ?>
-
+<?php if (isset($whoWeAre)) : ?>
+  <?= $this->insert('sections/image_text_alternating', ['page' => $whoWeAre]); ?>
 <?php endif;  ?>
-<!-- <div class="container">
-  <h1>Who we are</h1>
-  <div class="row">
-    <img class="col-md-6" style="padding: 15 px;" src="./assets/img/worship_hall_1.webp" alt="Worship at Wyreema Hall">
-    <div class="col-md-6">
-      <p> We are a non-denomination christian church that meets at a Wyreema Community Hall every Sunday morning for worship and Thursday night for bible study. Our Sunday worship is a capella Church of Christ style.
-      </p>
-    </div>
-  </div>
-  <div class="card card-plain card-blog">
-    <div class="row">
-      <div class="col-md-6">
-        <div class='card-body'>
-          <p class='card-description'> Paragraph 2
-          </p>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <img class="card card-image" style="padding: 15 px;" src="./assets/img/worship_hall_2.webp" alt="Worship at Wyreema Hall">
-      </div>
-    </div>
-  </div>
-</div> -->
 <?php $this->stop(); ?>
 
 <?php
@@ -73,58 +25,15 @@ $this->stop();
 ?>
 
 <?php $this->start('section3'); ?>
-<div class="container">
-  <h1>Community Involvement</h1>
-  <div class="row">
-    <div class="col-md-6">
-      <img class="card-image" style="padding: 15 px;" src="./assets/img/placeholder.jpg" alt="Community Involvement - Tony's Kitchen">
-    </div>
-    <div class="col-md-6">
-      <h2 class='text-center'>Tony's Kitchen</h2>
-      <p> Blankets and jumpers for teh homeless
-      </p>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-6">
-      <div class="card-body">
-
-        <h2 class='card-title'>Operation Christmas Child</h2>
-        <p> Paragraph 2
-        </p>
-      </div>
-    </div>
-    <img class="col-md-6" style="padding: 15 px;" src="./assets/img/Operation_christmas_Child.jpg" alt="Community Involvement - Operation Christmas Child">
-  </div>
-</div>
+<?php if (isset($communityInvolvement)) : ?>
+  <?= $this->insert('sections/image_text_alternating', ['page' => $communityInvolvement]); ?>
+<?php endif;  ?>
 <?php $this->stop(); ?>
 
 <?php $this->start('section4'); ?>
-<div class="container">
-  <h1>History</h1>
-  <div class="card card-plain card-blog">
-    <div class="row">
-      <div class="col-md-6"></div>
-      <img class="card card-image" style="padding: 15 px;" src="./assets/img/worship_home_1.jpg" alt="Community Involvement">
-    </div>
-    <div class="col-md-6">
-      <h2 class='text-center'>Worshiping in homes</h2>
-      <p> worshiping in homes
-      </p>
-    </div>
-  </div>
-</div>
-<div class="card card-plain card-blog">
-  <div class="row">
-    <div class="col-md-6">
-      <h2 class='text-center'>Worshiping in the hall</h2>
-      <p> worshiping in the hall
-      </p>
-    </div>
-    <img class="col-md-6" style="padding: 15 px;" src="./assets/img/worship_hall_3.jpg" alt="Community Involvement 2">
-  </div>
-</div>
-</div>
+<?php if (isset($history)) : ?>
+  <?= $this->insert('sections/image_text_alternating', ['page' => $history]); ?>
+<?php endif;  ?>
 <?php $this->stop(); ?>
 
 <b>Body content</b>
