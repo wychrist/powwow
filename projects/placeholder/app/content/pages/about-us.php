2b542e8476ba9b0e279b9c0662c6a063be8a2588
Christopher Kelly <w0072196@umail.uqs.edu.au>
2021-12-08 00:45:28 +1000

    Corrected error in app/pages/about_us.php where name changes had not been refactored.

--- projects/placeholder/app/content/pages/about-us.php
+++ projects/placeholder/app/content/pages/about-us.php
@@ -6,7 +6,7 @@ if (!defined('WYCHRIST_INIT')) {
 use App\Cms\Page;
 use App\Repository\PageRepository;
 
- $data = include_once content_dir('data/paper_2/about_us_template.php');
+$data = include_once content_dir('data/paper_2/about_us_template.php');
 
 /* $content = [
   'title' => 'About US',
@@ -31,17 +31,17 @@ $page =  new Page($content);
 
 $data = [
   'page' => $page,
-  'section1' => $section1
+  'whoWeAre' => $section1
 ]; */
 
-$data['page']->title = 'This is a new title'; // we are overriding the ttitle for the page
+$data['page']->title = 'This is a new title'; // we are overriding the title for the page
 
-$section1Children = $data['section1']->children;
+$whoWeAreChildren = $data['whoWeAre']->children;
 
 // we are overriding the content for the first page in section 1
-$section1Children[0]->content = 'We are working hard in wyreema and we are loving it';
+$whoWeAreChildren[0]->content = 'We are working hard in wyreema and we are loving it';
 
-$data['section1']->children = $section1Children;
+$data['whoWeAre']->children = $whoWeAreChildren;
 
 
 serve_template('templates/about_us_template', $data);
