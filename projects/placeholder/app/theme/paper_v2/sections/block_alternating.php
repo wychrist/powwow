c5df11dc49b3f4fcbdf774486174c2e9480e252c
Christopher Kelly <w0072196@umail.uqs.edu.au>
2021-12-07 23:50:59 +1000

    Added untracked files for previous commit. Fixed images URL moved from public/theme folder to general public folder.

---
+++ projects/placeholder/app/theme/paper_v2/sections/block_alternating.php
@@ -0,0 +1,14 @@
+<?php
+
+/*******************************************************
+  @params:
+    $content = Block object  
+
+ *************************************************************/ ?>
+
+<div class="container">
+  <h1 class="text-center title">Test</h1>
+  <pre>
+    <?= print_r($content); ?>
+  </pre>
+</div>
