b7e77c257aec0e44df2606c6c921b47080d355a4
Jibao Mansaray <jibaomansaray@gmail.com>
2022-01-12 11:14:28 +0000

    Add bin folder
    This folder contains splitsh-lit binary and a shell script.
    We use this script to split the  modules to their respective
    **readonly** modules

---
+++ bin/split.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# copied from: https://github.com/laravel/framework/blob/8.x/bin/split.sh
+
+set -e
+set -x
+
+CURRENT_BRANCH="0.1"
+
+function split()
+{
+    SHA1=`./bin/splitsh-lite --prefix=$1`
+    git push $2 "$SHA1:refs/heads/$CURRENT_BRANCH" -f
+}
+
+function remote()
+{
+    git remote add $1 $2 || true
+}
+
+git pull origin $CURRENT_BRANCH
+
+# register remote
+remote congregage_user git@github.com:wychrist/congregate_user.git
+
+
+# call split
+split 'projects/congregate/Modules/CongregateUser' congregate_user
