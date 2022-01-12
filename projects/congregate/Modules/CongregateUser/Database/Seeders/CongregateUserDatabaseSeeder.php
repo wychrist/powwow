8f0d9172aa7547849bb29b2c1539715054f21db0
Jibao Mansaray <jibaomansaray@gmail.com>
2022-01-12 11:13:46 +0000

    Create user module
    This module will contain user management features

---
+++ projects/congregate/Modules/CongregateUser/Database/Seeders/CongregateUserDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\CongregateUser\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class CongregateUserDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
