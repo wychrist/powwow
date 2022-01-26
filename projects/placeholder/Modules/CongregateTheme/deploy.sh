81bf86a77647a248ded8bb5d34bf368c021fb933
Jibao Mansaray <jibaomansaray@gmail.com>
2022-01-26 05:14:32 +0000

    Testing docs deploy script

---
+++ projects/placeholder/Modules/CongregateTheme/deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+# navigate into the build output directory
+cd docs/.vitepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:wychrist/congregate_theme.git master:gh-pages
+
+cd -
