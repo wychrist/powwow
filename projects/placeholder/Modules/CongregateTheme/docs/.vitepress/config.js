7d79f32d31d26dc9fb4f11cb4112c35fe4e18824
Jibao Mansaray <jibaomansaray@gmail.com>
2022-01-26 04:09:45 +0000

    Add vitepress for documentation

---
+++ projects/placeholder/Modules/CongregateTheme/docs/.vitepress/config.js
@@ -0,0 +1,7 @@
+import { defineConfig} from 'vitepress'
+
+export default defineConfig({
+    title: "Congregate Theme Module",
+    description: "The manages theming of a website",
+    base: '/congreage_user/'
+})
---
+++ projects/placeholder/Modules/CongregateTheme/docs/index.md
@@ -0,0 +1,1 @@
+# Congregate User Module
