dc7a50186054572d51c56887c18fdeda8dbb8eb1
Jibao Mansaray <jibaomansaray@gmail.com>
2022-01-26 04:58:26 +0000

    Add menu structure to the docs

--- projects/placeholder/Modules/CongregateTheme/docs/.vitepress/config.js
+++ projects/placeholder/Modules/CongregateTheme/docs/.vitepress/config.js
@@ -3,5 +3,53 @@ import { defineConfig} from 'vitepress'
 export default defineConfig({
     title: "Congregate Theme Module",
     description: "The manages theming of a website",
-    base: '/congregate_theme/'
+    base: '/congregate_theme/',
+
+    themeConfig: {
+        repo: 'wychrist/congregate_theme/',
+        docsDir: 'docs',
+        docsBranch: 'main',
+        editLinks: true,
+        editLinkText: 'Edit this on Github',
+        lastUpdated: 'Last Updated',
+
+        nav: [
+            { text: 'Guide', link: '/', activeMatch: '^/$|^/guide' },
+            { text: 'Config Reference', link: '/config/basics', activeMatch: '^/config/' }
+        ],
+        sidebar: {
+            '/guide/': getGuideSidebar(),
+            '/config/': getConfigSidebar(),
+            '/': getGuideSidebar()
+        }
+    }
 })
+
+
+function getGuideSidebar() {
+    return [
+        {
+            text: 'Introduction',
+            children: [
+                { text: 'What is Congregate Theme?', link: '/' },
+            ]
+        },
+        {
+            text: 'Advanced',
+            children: [
+                { text: 'Create a theme', link: '/guide/create_theme' }
+            ]
+        }
+    ];
+}
+
+function getConfigSidebar() {
+    return [
+        {
+            text: 'Configuration',
+            children: [
+                { text: 'Basics', link: '/config/basics' }
+            ]
+        },
+    ];
+}
---
+++ projects/placeholder/Modules/CongregateTheme/docs/.vitepress/dist/assets/config_basics.md.2eb8741e.js
@@ -0,0 +1,1 @@
+import{_ as a,c as t,o as e}from"./app.c92badad.js";const f='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"config/basics.md","lastUpdated":1643172743291}',s={};function c(r,o,n,p,d,i){return e(),t("div")}var l=a(s,[["render",c]]);export{f as __pageData,l as default};
