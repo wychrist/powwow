3fc117129200134589a2612659ca2e48ba1343f5
Jibao Mansaray <jibaomansaray@gmail.com>
2022-02-15 11:57:28 +0000

    Use function namespace

--- projects/placeholder/Modules/CongregateSetting/Database/Migrations/2022_02_01_103732_insert_defaults_into_settings.php
+++ projects/placeholder/Modules/CongregateSetting/Database/Migrations/2022_02_01_103732_insert_defaults_into_settings.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use function Modules\CongregateSetting\set_settings;
 
 class InsertDefaultsIntoSettings extends Migration
 {
