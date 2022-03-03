bf6ec03f79d24ea04e6594afda209b20dc22151f
Christopher Kelly <w0072196@umail.uqs.edu.au>
2022-03-03 20:36:51 +1000

    Adding missing files

---
+++ projects/placeholder/resources/views/themes/paper_v2/sections/home_about_us.blade.php
@@ -0,0 +1,19 @@
+<div class="container">
+  <h1>About Us</h1>
+  <div class="card card-plain card-blog">
+    <div class="row">
+      <div class="col-md-6">
+        <div class='card-body'>
+          <p class='card-description'>We are ... a description, a description, a description, a description, a description, a description, a description, a description, a description, a description. </p>
+          <p class='card-description'>Wyreema Christians church meets every Sunday at the <a href="https://www.facebook.com/WyreemaCommunityHall/" rel="tooltip" title="Wyreema Community Hall" data-placement="bottom" target="_blank">
+              Wyreema Community Hall
+            </a> from 9:15 am for a 9:30 am start.</p>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d949.6407255835479!2d151.85567642799563!3d-27.656602834335718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b964f902b9e70fb%3A0x6005a22abcb17bc4!2sWyreema%20Hall!5e0!3m2!1sen!2sau!4v1637761022483!5m2!1sen!2sau" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy">
+        </iframe>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ projects/placeholder/resources/views/themes/paper_v2/sections/latest_posts.blade.php
@@ -0,0 +1,45 @@
+<div class="container">
+  <h1 class="title">The Latest</h1>
+  <p></p>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card" data-background="image" style="background-image: url('/assets/img/aaron-burden-9zsHNt5OpqE-unsplash.jpg')">
+        <div class="card-body">
+          <h6 class="card-category">Sunday Worship 13/02/22</h6>
+          <p class="card-description">A lesson title/description</p>
+          <div class="card-footer">
+            <a href="javascript:;" class="btn btn-danger btn-round card-link">
+              <i class="fa fa-book" aria-hidden="true"></i> Read Article
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card" data-background="image" style="background-image: url('/assets/img/aaron-burden-9zsHNt5OpqE-unsplash.jpg')">
+        <div class="card-body">
+          <h6 class="card-category">Sunday Worship 20/02/22</h6>
+          <p class="card-description">A Different lesson title/description</p>
+          <div class="card-footer">
+            <a href="javascript:;" class="btn btn-danger btn-round card-link">
+              <i class="fa fa-book" aria-hidden="true"></i> Read Article
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card" data-background="image" style="background-image: url('/assets/img/aaron-burden-9zsHNt5OpqE-unsplash.jpg')">
+        <div class="card-body">
+          <h6 class="card-category">Sunday Worship 27/02/22</h6>
+          <p class="card-description">A very very long and interesting lesson title/description</p>
+          <div class="card-footer">
+            <a href="javascript:;" class="btn btn-danger btn-round card-link">
+              <i class="fa fa-book" aria-hidden="true"></i> Read Article
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
