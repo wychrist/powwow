573a8b40b7ab99d849ddfc434cc1f6f5c67d0700
Jibao Mansaray <jibaomansaray@gmail.com>
2022-03-08 10:07:29 +0000

    Move contents' routes to the content module

--- projects/placeholder/routes/web.php
+++ projects/placeholder/routes/web.php
@@ -21,12 +21,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'indexAction'])->name('index');
 
-// Pages handler
-Route::get('/pages/{id}', [PageController::class, 'indexAction'])->name('pages');
-
-// Post handler
-Route::get('posts/{id}', [PostController::class, 'indexAction'])->name('posts');
-
 // contact us
 Route::get('/contact-us', [ContactController::class, 'indexAction'])->name('contact_us_form');
 Route::post('/contact-us', [ContactController::class, 'handleAction'])->name('contact_us_submitted');
