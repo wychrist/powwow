154d3ea2c65cf637f1e03a4b3825253f5bf113cc
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-05 15:52:51 +0000

    Copied over code from the contact us layout

--- projects/placeholder/resources/views/themes/paper_v2/layouts/main.blade.php
+++ projects/placeholder/resources/views/themes/paper_v2/layouts/main.blade.php
@@ -1,10 +1,43 @@
+<!--
+=========================================================
+* Paper Kit Pro - v2.3.1
+=========================================================
 
-@extends('base_theme::layouts.one_column')
+* Product Page: https://www.creative-tim.com/product/paper-kit-2-pro
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
 
-@section('content')
-    <h1>Paper 2 layouts</h1>
+Coded by www.creative-tim.com
 
-    <p>
-        Hello world org.
-    </p>
-@endsection
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  @include('theme_section::header_css')
+</head>
+
+<body class="index-page sidebar-collapse">
+
+  @include('theme_section::header')
+
+  <div class="main wrapper">
+    <div class='section section-brown'>
+        <div class="container" style="margin-top: 150px">
+            @include("theme::fragments.flash_message.index")
+            @yield('content', '@section("content")')
+        </div>
+    </div>
+
+    <footer class="section-dark footer footer-black">
+      @include('theme_section::footer')
+    </footer>
+
+    @include('theme_section::footer_js')
+
+</body>
+
+</html>
