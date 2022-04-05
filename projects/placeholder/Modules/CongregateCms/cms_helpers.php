274fc802a27b84ff0686d18f91ec6c0af48e8823
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-05 15:58:35 +0000

    Use `route` to generate full url

--- projects/placeholder/Modules/CongregateCms/cms_helpers.php
+++ projects/placeholder/Modules/CongregateCms/cms_helpers.php
@@ -2,12 +2,16 @@
 
 if(!function_exists('cms_page_url')) {
     function cms_page_url(string|int $slugOrId) {
-        return config()->get('congregatecms.pages_endpoint', 'posts') . "/{$slugOrId}";
+        return route('congregatecms.a_page', [
+            'id' => $slugOrId
+        ]);
     }
 }
 
 if(!function_exists('cms_post_url')) {
     function cms_post_url(string|int $slugOrId) {
-        return config()->get('congregatecms.posts_endpoint', 'posts') . "/{$slugOrId}";
+        return route('congregatecms.a_posts', [
+            'id' => $slugOrId
+        ]);
     }
 }
