c60181b5cc3d2bfaff316750347a602514658812
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-05 16:10:31 +0000

    paper_v2 is now overriding the default post template

---
+++ projects/placeholder/resources/views/themes/paper_v2/modules/congregatecms/templates/post_default.blade.php
@@ -0,0 +1,16 @@
+<?php
+  $page = $post; // we hard coded "page" in some sections
+?>
+@extends('theme::layouts.one_column')
+
+@section('content')
+    <h1>{{ $post->title }}</h1>
+    <img src="{{$post->poster}}" style="width: 300px" />
+    <hr />
+    <i>
+        {!! $post->intro !!}
+    </i>
+    <hr/>
+        {!! $post->content !!}
+@endsection
+
