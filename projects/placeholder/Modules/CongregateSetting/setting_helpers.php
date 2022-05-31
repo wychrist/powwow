aaf3acbfbeff8906803e096b4af69b9ddb2e96b8
Jibao Mansaray <jibaomansaray@gmail.com>
2022-05-31 11:05:46 +0000

    Gracefully fallback to the config in the filesystem
    When we cannot connect to the database, gracefully fallback
    to Laravel configs.

--- projects/placeholder/Modules/CongregateSetting/setting_helpers.php
+++ projects/placeholder/Modules/CongregateSetting/setting_helpers.php
@@ -8,25 +8,29 @@ use Modules\CongregateSetting\Entities\Setting;
 if (!function_exists('settings')) {
     function settings(string $name = '', $default = null): mixed
     {
-        static $collection = null;
-        if ($collection == null) {
-            $collection = Cache::rememberForever('settings_cache', function () {
-                $built = [];
-                Setting::all()->each(function ($setting) use (&$built) {
-                    if (!isset($built[$setting->module])) {
-                        $built[$setting->module] = [];
-                    }
-                    $built[$setting->module][$setting->name] = $setting->value;
+        try {
+            static $collection = null;
+            if ($collection == null) {
+                $collection = Cache::rememberForever('settings_cache', function () {
+                    $built = [];
+                    Setting::all()->each(function ($setting) use (&$built) {
+                        if (!isset($built[$setting->module])) {
+                            $built[$setting->module] = [];
+                        }
+                        $built[$setting->module][$setting->name] = $setting->value;
+                    });
+                    return $built;
                 });
-                return $built;
-            });
-        }
+            }
 
-        if (is_array($name)) {
-            return set_settings($name);
-        }
+            if (is_array($name)) {
+                return set_settings($name);
+            }
 
-        return walk_settings($collection, explode('.', $name), config($name, $default));
+            return walk_settings($collection, explode('.', $name), config($name, $default));
+        } catch (\Exception $_) {
+            return config($name, $default);
+        }
     }
 }
 
@@ -51,32 +55,35 @@ if (!function_exists('walk_settings')) {
 if (!function_exists('set_settings')) {
     function set_settings($key, $value = null)
     {
-        $keyValue = (is_array($key)) ? $key : [$key => $value];
-        foreach ($keyValue as $name => $value) {
-            $pieces = explode('.', $name);
-            if (count($pieces) >= 2) {
-                $setting = Setting::where([
-                    'module' => $pieces[0],
-                    'name' => $pieces[1]
-                ])->firstOrNew([
-                    'module' => $pieces[0],
-                    'name' => $pieces[1]
-                ]);
+        try {
+            $keyValue = (is_array($key)) ? $key : [$key => $value];
+            foreach ($keyValue as $name => $value) {
+                $pieces = explode('.', $name);
+                if (count($pieces) >= 2) {
+                    $setting = Setting::where([
+                        'module' => $pieces[0],
+                        'name' => $pieces[1]
+                    ])->firstOrNew([
+                        'module' => $pieces[0],
+                        'name' => $pieces[1]
+                    ]);
 
-                if (is_array($value)) {
-                    if (is_array($setting->value)) {
-                        $setting->value = array_merge($setting->value, $value);
+                    if (is_array($value)) {
+                        if (is_array($setting->value)) {
+                            $setting->value = array_merge($setting->value, $value);
+                        } else {
+                            $setting->value = $value;
+                        }
                     } else {
                         $setting->value = $value;
                     }
-                } else {
-                    $setting->value = $value;
+
+                    $setting->save();
                 }
-
-                $setting->save();
             }
-        }
 
-        Cache::flush('settings_cache');
+            Cache::flush('settings_cache');
+        } catch (\Exception $_) {
+        }
     }
 }
