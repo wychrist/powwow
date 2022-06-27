b6fcfe965f83c07a501018d2220e6b153e761ce6
Jibao Mansaray <jibaomansaray@gmail.com>
2022-06-27 12:53:17 +0000

    Allow the user to hook into the pre and post build steps

--- projects/placeholder/docker/run.sh
+++ projects/placeholder/docker/run.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
+if [ -f /var/www/deploy_scripts/pre.sh ]
+then
+    sh /var/www/deploy_scripts/pre.sh
+fi
+
 cd /var/www
 
 php artisan cache:clear
-php artisan route:cache
 php artisan migrate
+php artisan route:cache
+php artisan view:cache
+
+if [ -f /var/www/deploy_scripts/post.sh ]
+then
+    sh /var/www/deploy_scripts/post.sh
+fi
 
 /usr/bin/supervisord -c /etc/supervisord.conf
