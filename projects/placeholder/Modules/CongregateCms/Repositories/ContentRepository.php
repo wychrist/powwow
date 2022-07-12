f5c15d06ea09ed9ae16910ee6584c49eaef70e34
Jibao Mansaray <jibaomansaray@gmail.com>
2022-07-12 13:27:24 +0000

    Add content repository
    This is how the outside world access that from this module

---
+++ projects/placeholder/Modules/CongregateCms/Repositories/ContentRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Modules\CongregateCms\Repositories;
+
+use App\Cms\Page;
+
+class ContentRepository
+{
+
+    public function getLatestPost(): Page
+    {
+        // $default = new Page(['title' => 'We are cooking something', 'intro' => '', 'content' => 'A post will be out here soon']);
+        return include_once content_dir('data/posts/latest.php');
+    }
+
+    public function findAPost(string | int $idOrSlug): Page | null
+    {
+        return (is_numeric($idOrSlug)) ? $this->findPostById($idOrSlug) : $this->findPostBySlug($idOrSlug);
+    }
+
+    public function findPostById(int $id): Page | null
+    {
+        try {
+            return require(content_dir("data/posts/{$id}.php"));
+        } catch (\Exception $_) {
+            return null;
+        }
+    }
+
+    public function findPostBySlug(string $slug): Page | null
+    {
+        try {
+            return collect(content_dir('data/posts/list.php'))->sole(function ($_, $value) use ($slug) {
+                return $value->slug == $slug;
+            });
+        } catch (\Exception $_) {
+            return null;
+        }
+    }
+
+    public function findAPage(string | int $idOrSlug): Page | null
+    {
+        return (is_numeric($idOrSlug)) ? $this->findPageById($idOrSlug) : $this->findPageBySlug($idOrSlug);
+    }
+
+    public function findPageById(int $id): Page | null
+    {
+        try {
+            return require(content_dir("data/pages/{$id}.php"));
+        } catch (\Exception $_) {
+            return null;
+        }
+    }
+
+    public function findPageBySlug(string $slug): Page | null
+    {
+        try {
+            return collect(content_dir('data/pages/list.php'))->sole(function ($_, $value) use ($slug) {
+                return $value->slug == $slug;
+            });
+        } catch (\Exception $_) {
+            return null;
+        }
+    }
+}
