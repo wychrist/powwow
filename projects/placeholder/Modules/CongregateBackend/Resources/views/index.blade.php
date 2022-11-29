100d63d06848683d4bc7004beb0200bc20f85805
Jibao Mansaray <jibaomansaray@gmail.com>
2022-11-29 11:39:06 +0000

    Remove the `content` section
    This will cause the theme to show the dashboard by default.

--- projects/placeholder/Modules/CongregateBackend/Resources/views/index.blade.php
+++ projects/placeholder/Modules/CongregateBackend/Resources/views/index.blade.php
@@ -1,10 +1,2 @@
 
 @extends('backend_theme::layouts.two_columns_left')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('congregatebackend.name') !!}
-    </p>
-@endsection
