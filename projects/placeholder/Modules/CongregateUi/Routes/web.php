3b9085808e20f6fbb74c6199cbf2f4a73bda0d7a
Jibao Mansaray <jibaomansaray@gmail.com>
2022-12-06 11:59:21 +0000

    Give route name

--- projects/placeholder/Modules/CongregateUi/Routes/web.php
+++ projects/placeholder/Modules/CongregateUi/Routes/web.php
@@ -12,5 +12,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('congregateui')->group(function() {
-    Route::get('/', 'CongregateUiController@index');
+    Route::get('/', 'CongregateUiController@index')->name('congregateui');
 });
