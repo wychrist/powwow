e6ed992e4f8eb0af2bb6660043c841b4055bac2a
Jibao Mansaray <jibaomansaray@gmail.com>
2023-02-09 12:14:07 +0000

    Add MenuLevelTransition event class
    This event is dispatched when we are building the backend menu.

---
+++ projects/placeholder/Modules/CongregateTheme/Events/Backend/MenuLevelTransition.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Modules\CongregateTheme\Events\Backend;
+
+use Illuminate\Queue\SerializesModels;
+
+class MenuLevelTransition
+{
+    use SerializesModels;
+
+    private $handled = false;
+    private $builtHtml = "";
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(private array $entries, private int $level = 0)
+    {
+    }
+
+    public function getEntries(): array
+    {
+        return $this->entries;
+    }
+
+    public function getLevel(): int
+    {
+        return $this->level;
+    }
+
+    public function setHtml(string $html)
+    {
+        $this->builtHtml = $html;
+    }
+
+    public function getHtml(): string
+    {
+        return $this->builtHtml;
+    }
+
+    public function setHandled($handled)
+    {
+        $this->handled = $handled;
+    }
+
+    public function wasHandled()
+    {
+        return $this->handled;
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return [];
+    }
+}
