c5ca3d47384d08890f03824bf81bfd191284e37a
Jibao Mansaray <jibaomansaray@gmail.com>
2023-02-09 12:19:50 +0000

    move `walk_backend_menu_item` here

--- projects/placeholder/Modules/CongregateTheme/theme_helpers.php
+++ projects/placeholder/Modules/CongregateTheme/theme_helpers.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Event;
+use Modules\CongregateTheme\Events\Backend\MenuLevelTransition;
+
 if (!function_exists('app_root_dir')) {
     function app_root_dir(string $append = ''): string
     {
@@ -45,17 +48,18 @@ if (!function_exists('get_data')) {
 if (!function_exists('theme_dir')) {
     function theme_dir(string $append = ''): string
     {
-        return config('congregatetheme.themes_directory', '') . (($append) ? '/'. $append: '');
+        return config('congregatetheme.themes_directory', '') . (($append) ? '/' . $append : '');
     }
 }
 
-if(!function_exists('inject_template_data')) {
-    function inject_template_data($data) {
-        if(isset($data['content'])) {
+if (!function_exists('inject_template_data')) {
+    function inject_template_data($data)
+    {
+        if (isset($data['content'])) {
             // View::share('content', $data['content']);
         }
 
-        if(!isset($data['setting'])) {
+        if (!isset($data['setting'])) {
             // View::share('setting', app(SettingInterface::class));
         }
 
@@ -80,8 +84,9 @@ if (!function_exists('render_backend_template')) {
     }
 }
 
-if(!function_exists('custom_template')) {
-    function custom_template(string $template, array $data = []) {
+if (!function_exists('custom_template')) {
+    function custom_template(string $template, array $data = [])
+    {
         $data = inject_template_data($data);
         return view($template, $data)->render();
     }
@@ -97,5 +102,44 @@ if (!function_exists('serve_backend_template')) {
     function serve_backend_template(string $template, array $data = [])
     {
         echo render_backend_template($template, $data);
+    }
+}
+
+if (!function_exists('walk_backend_menu_item')) {
+
+    function walk_backend_menu_item($entries, int $level): string
+    {
+
+        $builtMenu = "";
+
+        $event  = new MenuLevelTransition($entries, $level);
+        Event::dispatch($event);
+        $builtMenu = $event->getHtml();
+        if ($builtMenu) {
+            return $builtMenu;
+        }
+
+        foreach ($entries as $item) {
+            $children = $item->getChildren();
+            $active = ($item->isActive() || ($level == 0 && $item->getHasActiveChild())) ? 'active' : '';
+            $open = ($active) ? 'menu-open' : '';
+            $link = $item->getLink();
+            $icon = $item->getIcon() ?? 'unknown-icon';
+
+            $builtMenu .= "<li class=\"nav-item {$open}\">
+                        <a href=\"{$link}\" class=\"nav-link {$active}\">
+                        <i class=\" {$icon} nav-icon\"></i>
+                        <p>{$item->getLabel()}</p>
+                        </a>";
+
+            if ($children) {
+                $builtMenu .=  '<ul class="nav nav-treeview">';
+                $builtMenu .=    walk_backend_menu_item($children, $level + 1);
+                $builtMenu .=  '</ul>';
+            }
+            $builtMenu .=  '</li>';
+        }
+
+        return $builtMenu;
     }
 }
