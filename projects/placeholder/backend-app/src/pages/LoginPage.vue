88df5cd858b13c110f6c0bb39552d8e936d05afb
Christopher Kelly <w0072196@usq.edu.au>
2023-04-06 23:43:15 +1000

    Started login page. Non functional.

---
+++ projects/placeholder/backend-app/src/pages/LoginPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-card flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Login</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-card-section>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </q-card-section>
+      <q-separator />
+      <q-card-section>
+          <q-card-actions align="around">
+            <q-btn flat>Action 1</q-btn>
+            <q-btn flat>Action 2</q-btn>
+          </q-card-actions>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
