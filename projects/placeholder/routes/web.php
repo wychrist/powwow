a3a2b03e78e856e6c26f66d98079e0569732f317
Jibao Mansaray <jibaomansaray@gmail.com>
2023-04-20 11:12:43 +0000

    Add some test routes

--- projects/placeholder/routes/web.php
+++ projects/placeholder/routes/web.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\NewsletterController;
 use App\Mail\OnlineContact\ValidateEmail;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Kpama\Easybuilder\Lib\Api\RouteBuilder;
 use Modules\CongregateContract\Theme\FlashMessageInterface;
 
 /*
@@ -28,13 +30,42 @@ Route::post('/contact-us', [ContactController::class, 'handleAction'])->name('co
 // newsletter
 Route::post('/newsletter-do-subscribe', [NewsletterController::class, 'handleAction'])->name('newsletter_subscribe');
 
-Route::get('/menu', function() {
+Route::get('/menu', function () {
     $menu = require_once(app_root_dir('content/data/menus/home_menu.php'));
     return $menu->toArray();
 });
 
+// @todo this suppose to be under "backend/..."
+// this is the original dashboard Laravel provides
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+// sanctum
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+
+
+RouteBuilder::generate(User::class);
+
+require __DIR__ . '/auth.php';
