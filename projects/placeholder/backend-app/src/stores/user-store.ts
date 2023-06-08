1c0a20f17e9b87a3b02e3932fd3d483317b739f0
Jibao Mansaray <jibaomansaray@gmail.com>
2023-06-08 10:22:38 +0000

    Backend UI: Implement `user store`

--- projects/placeholder/backend-app/src/stores/user-store.ts
+++ projects/placeholder/backend-app/src/stores/user-store.ts
@@ -1,7 +1,34 @@
 import { defineStore } from 'pinia'
+import { readonly, ref } from 'vue'
+import { useAuthStore } from './auth-store'
 
-export const useUserStore = defineStore('user-store', {
-  state: () => ({
+export interface IUser {
+  email: string,
+  name: string,
+  created_at: string,
+  updated_at: string
+}
 
-  })
+function makeUser (): IUser {
+  return {
+    email: '',
+    name: '',
+    created_at: '',
+    updated_at: ''
+  }
+}
+
+export const useUserStore = defineStore('user-store', () => {
+  const user = ref<IUser>(makeUser())
+  const auth = useAuthStore()
+
+  async function fetchMe () {
+    const response = await auth.http.get('/api/v1/me')
+    console.log('response for me', response)
+  }
+
+  return {
+    user: readonly(user),
+    fetchMe
+  }
 })
