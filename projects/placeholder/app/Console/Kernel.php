e5c33d1c5213172f3207aba2a143054271a21f03
Jibao Mansaray <jibaomansaray@gmail.com>
2023-08-03 12:17:44 +0000

    Change the cron job to run every 6 months
    We can use this list to block spammers for a period of time.

--- projects/placeholder/app/Console/Kernel.php
+++ projects/placeholder/app/Console/Kernel.php
@@ -16,7 +16,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('congregate:email-prune')->daily();
+        // $schedule->command('congregate:email-prune')->daily();
+        $schedule->command('congregate:email-prune')->cron("* * * */6 *");
     }
 
     /**
@@ -26,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
